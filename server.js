ab7a22d1dfdd3258f25e93c404a4673b9335f924
KarvaMies <jeremias.moberg@gmail.com>
2022-11-13 16:46:16 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+let hello = [
+    {msg: "Hello world"}
+]
+
+const express = require("express");
+const os = require("os")
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req, res) => {
+    res.json(hello)
+})
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!!</h1>");
+});
+
+app.listen(port, () => console.log("Server is listening port " + port + "!"));
+
+
+/*
+const http = require("http")
+
+console.log("Server is running...")
+
+http.createServer(function(req, res) {
+    res.write("Hello World!");
+    res.end();
+    console.log("Browser has reached us!")
+}).listen(3000);
+*/
