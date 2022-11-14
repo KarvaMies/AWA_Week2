9d07480206acc9e61257471b8e90dd338e0d4426
KarvaMies <jeremias.moberg@gmail.com>
2022-11-14 13:47:22 +0200

    tasks 1 + 2

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-let hello = [
-    {msg: "Hello world"}
-]
+let hello = {msg: "Hello world"}
 
 const express = require("express");
 const os = require("os")
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!!</h1>");
+});
+
 app.get("/hello", (req, res) => {
     res.json(hello)
 })
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!!</h1>");
+app.get("/echo/:id", (req, res) => {
+    let obj = {id: req.params.id}
+    res.send(obj);
 });
 
 app.listen(port, () => console.log("Server is listening port " + port + "!"));
